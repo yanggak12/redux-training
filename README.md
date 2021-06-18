d03d99cf847d73e3a2b273a863b5b4b0f2069cc9
양윤상 <yanggak12@naver.com>
2021-06-19 02:34:39 +0900

    Store and Reducer

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 Training Vanilla-Redux !!
 (nomad coder - 초보자를 위한 리덕스 101)
+
+## 1. Store
+
+    - 너의 데이터를 넣는 곳(state)
+    - state는 너의 앱에서 바뀌는 데이터
+    - createstore는 반드시 reducer를 매개변수로 넣어줘야한다.
+
+## 2. reducer
+
+    - reducer는 너의 데이터를 변경해주는 함수
+    - reducer만이 너의 데이터를 변경할 수 있어
+    - reducer의 데이터를 변경하는 것은 action !
--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-const updateText = () => {
-  number.innerText = count;
+//reducer - 이 함수만 state를 변경할 수 있다! 이것이 리덕스의 매력
+const countModifier = (count = 0) => {
+  // return your data
+  return count;
 };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
