c4b153942f10cff2d9fed99288dfa118832529c7
양윤상 <yanggak12@naver.com>
2021-06-19 01:56:40 +0900

    #1.0 Vanilla Counter

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count++;
+  updateText();
+};
+
+const handleMinus = () => {
+  count--;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+
+minus.addEventListener("click", handleMinus);
