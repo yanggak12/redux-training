6ad9d7737066e68761f9841ef34eb9de798bfa20
양윤상 <yanggak12@naver.com>
2021-06-27 21:08:29 +0900

    Action & Dispatch

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ Training Vanilla-Redux !!
     - reducer는 너의 데이터를 변경해주는 함수
     - reducer만이 너의 데이터를 변경할 수 있어
     - reducer의 데이터를 변경하는 것은 action !
+
+## 3. Action
+
+    - action은 Reducer가 어떻게 데이터를 변경할지 알려준다.
+    - action은 reducer에 어떻게 데이터를 변경할 지 알려주기 위해 dispatch의 도움을 받는다.
+
+## 4. Dispatch
+
+    - dispatch는 reducer에 action을 전달해준다.
+    - dispatch는 반드시 객체형이어야 한다.
--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 //reducer - 이 함수만 state를 변경할 수 있다! 이것이 리덕스의 매력
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
   // return your data
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
   return count;
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
