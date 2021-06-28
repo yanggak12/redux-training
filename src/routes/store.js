3725a61f4563089783be5923a64ca29c9e8b510e
양윤상 <yanggak12@naver.com>
2021-06-28 15:13:13 +0900

    CreateReducer

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,19 +1,28 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addTodo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteTodo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addTodo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteTodo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], {
+  [addTodo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteTodo]: (state, action) => {
+    return state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 
